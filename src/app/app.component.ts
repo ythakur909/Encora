8f28978e21139865501faf1efee8c82c716a4f5e
Yogesh Thakur <ythkur909@gmail.com>
2021-12-27 21:46:48 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  profileForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    id: new FormControl(''),
+  });
   title = 'encora_project';
+  contacts: any;
+  isFormVisible = false;
+  isUpdate = false;
+  apiPath =
+    'https://my-json-server.typicode.com/voramahavir/contacts-mock-response/contacts';
+
+  ngOnInit(): void {
+    this.http.get(this.apiPath).subscribe((res) => {
+      this.contacts = res;
+    });
+  }
+
+  onAddForm() {
+    this.isFormVisible = true;
+    this.isUpdate = false;
+  }
+
+  onEdit(event: any) {
+    this.isUpdate = true;
+    this.isFormVisible = true;
+    this.profileForm.patchValue(event);
+  }
+
+  onDelete(event: any) {
+    this.contacts.splice(event, 1);
+  }
+
+  onSave() {
+    this.profileForm.controls['id'].setValue(this.contacts.length + 1);
+    this.contacts.push(this.profileForm.value);
+    this.onCancel();
+  }
+
+  onUpdate() {
+    this.contacts.forEach((element: any, index: any) => {
+      if (element.id === this.profileForm.value.id) {
+        this.contacts[index] = this.profileForm.value;
+      }
+    });
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.isFormVisible = false;
+    this.profileForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
